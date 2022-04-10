997d0e9e4796b3c104764503823d3631bc6109bf
brckbhatta <103332320+brckbhatta@users.noreply.github.com>
2022-04-10 12:16:55 +0530

    Add files via upload

---
+++ brickproject/src/main/java/com/brick/brickproject/BrickprojectApplication.java
@@ -0,0 +1,13 @@
+package com.brick.brickproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickprojectApplication.class, args);
+	}
+
+}
---
+++ brickproject/src/test/java/com/brick/brickproject/BrickprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brick.brickproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
